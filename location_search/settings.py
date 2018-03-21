f75cfd5d50ffcfa0e265e7361ce6aff430bafcac
trustyred <244073249@qq.com>
2018-03-21 20:58:32 +0800

    add local static directory so make js file and css file localize

--- location_search/settings.py
+++ location_search/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -122,6 +121,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_ROOT = '/mnt/django/static'
 STATIC_URL = '/static/'
+STATIC_ROOT = "/mnt/django/static"
+
---
+++ main_site/static/map.js
@@ -0,0 +1,184 @@
+var map = null;
+    
+    var currentPoint = {
+                latAndLong: [116.404, 39.915],
+                name: "这是默认的位置"
+            };
+            var viewRange = {
+                lng_left:0,
+                lng_right:360,
+                lat_left:0,
+                lat_right:360
+            };
+            //在地图中心设置标注点
+            var pos_left = $(window).width()/2-28;
+            var pos_top = ($(window).height()-64)/2-28;
+            $('#mark').css({
+                top:pos_top,
+                left:pos_left
+            });
+
+            //点击用户图标出现侧栏
+            $('#user').click(function(){
+                $('#user-centered').fadeIn('slow');
+            });
+            $('#back').click(function(){
+                $('#user-centered').fadeOut('slow');
+            })
+
+            //注册登录页面的显示和隐藏
+            $('#login-register').bind('click',function(){
+                $('#user-centered').fadeOut('slow');
+                $('#shade-login').fadeIn('slow');
+                
+            });
+            $('#login-entrance').bind('click',function(){
+                $('#shade-login').show();
+                $('#shade-register').hide();
+            });
+            $('#register-entrance').bind('click',function(){
+                $('#shade-register').show();
+                $('#shade-login').hide();
+            })
+            //使用谷歌原生api获取地理位置信息
+            function getLocation_useGeo(callBack){
+                if(navigator.geolocation){
+                    navigator.geolocation.getCurrentPosition(function (position) {
+                        var lat = position.coords.latitude; //纬度
+                        var lng = position.coords.longitude; //经度
+                        console.log(lat+lng+'test2');
+                        var gpsPoint = new BMap.Point(lng, lat);
+                        var convertor = new BMap.Convertor();
+                        var pointArr = [];
+                        pointArr.push(gpsPoint);
+                        convertor.translate(pointArr, 1, 5, function (data) {
+
+                        currentPoint.latAndLong = (data.points[0].lng + ',' + data.points[0].lat).split(',');
+                        console.log('before callback');
+                        callBack();
+
+                    });
+                    },function () {
+                        getLocation_useBaiDuApi(mapCreate);
+                        document.querySelector(".btn-get").addEventListener('click', function() {
+                            getLocation_useBaiDuApi(reLocate);
+                        });
+                    });
+                }
+                else{
+                    alert('不支持谷歌原生定位api')
+                }
+            }
+            //使用百度api获取地理位置信息（但是获取位置不准确）
+            function getLocation_useBaiDuApi(callBack) {
+                var geolocation = new BMap.Geolocation();
+                geolocation.getCurrentPosition(function(r){
+                if(this.getStatus() == BMAP_STATUS_SUCCESS){
+                    var gpsPoint = new BMap.Point(r.point.lng, r.point.lat);
+                    var convertor = new BMap.Convertor();
+                    var pointArr = [];
+                    pointArr.push(gpsPoint);
+                    convertor.translate(pointArr, 1, 5, function (data) {
+                        currentPoint.latAndLong = (data.points[0].lng + ',' + data.points[0].lat).split(',')
+                        callBack();
+                        document.querySelector(".btn-get").addEventListener('click', function() {
+                            getLocation_useBaiDuApi(reLocate);
+                        });
+                    });
+
+                }
+                else {
+                    alert('failed'+this.getStatus());
+                }
+            });
+            }
+
+            function mapCreate() {
+
+                map = new BMap.Map("showmap");
+                console.log("map info: ");
+                console.log(map)
+                var point = new BMap.Point(currentPoint.latAndLong[0], currentPoint.latAndLong[1]);
+                map.centerAndZoom(point ,14);
+                map.enableScrollWheelZoom(true);
+                var myIcon = new BMap.Icon("http://123.207.31.244:8080/static/img/peoicon.png", new BMap.Size(30, 30)); //更换图标
+                var marker = new BMap.Marker(point, {
+                    icon: myIcon
+                }); // 创建标注
+                map.addOverlay(marker); // 将标注添加到地图中
+                getViewRange(function () {
+                    var point = new BMap.Point(117.950,41.000);
+                    var myIcon = new BMap.Icon("http://123.207.31.244:8080/static/peoicon.png", new BMap.Size(30, 30)); //更换图标
+                    var marker = new BMap.Marker(point,{
+                        icon : myIcon
+                    });
+                    map.addOverlay(marker)
+                })
+                getViewRange(getBikeLocation)
+            }
+            function reLocate(){
+                map.clearOverlays();
+                var point = new BMap.Point(currentPoint.latAndLong[0], currentPoint.latAndLong[1]);
+                var myIcon = new BMap.Icon("http://123.207.31.244:8080/static/img/peoicon.png", new BMap.Size(30, 30)); //更换图标#}
+                var marker = new BMap.Marker(point, {
+                    icon: myIcon
+                }); // 创建标注
+                map.addOverlay(marker); // 将标注添加到地图中
+                map.centerAndZoom(point,15);
+                map.panTo(point);
+                getViewRange(getBikeLocation);
+                console.log('test')
+            }
+            function getViewRange(callBack){
+                var range = map.getBounds();
+                var sw = range.getSouthWest();//获取左下角（西南）
+                var ne = range.getNorthEast();//获取右上角（东北）
+                viewRange.lat_left = sw.lat;
+                viewRange.lat_right = ne.lat;
+                viewRange.lng_left = sw.lng;
+                viewRange.lng_right = ne.lng;
+                console.log(viewRange);
+                callBack()
+            }
+            function getBikeLocation(){
+
+                $.ajax({
+                    type:'POST',
+                    url:'main/getBikeLatLng',
+                    data:{
+                        "lon_left":viewRange.lng_left,
+                        "lon_right":viewRange.lng_right,
+                        "lat_left":viewRange.lat_left,
+                        "lat_right":viewRange.lat_right
+                    },
+                    success : function (data) {
+                        console.log('success');
+                        var bikeList = jQuery.parseJSON(data);
+                        for(var i=0;i<bikeList.length;i++){
+                            console.log(bikeList[i]);
+                            var myIcon = new BMap.Icon("http://123.207.31.244:8080/bike.png", new BMap.Size(50, 50)); //更换图标
+                            var point = new BMap.Point(bikeList[i].bikeLongitude,bikeList[i].bikeLatitude);
+
+                            var marker = new BMap.Marker(point,{
+                                icon:myIcon
+                            });
+                            map.addOverlay(marker)
+                        }
+                    },
+                    error : function () {
+                        console.log('error')
+                    }
+                })
+
+            }
+         $(document).ready(function(){
+
+
+         console.log("create");
+         getLocation_useGeo(mapCreate);
+         document.querySelector(".btn-get").addEventListener('click', function() {
+                           getLocation_useGeo(reLocate);
+          });
+
+         })
+
