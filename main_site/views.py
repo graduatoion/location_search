715bda06f2b8ebfb4c5952605521fdcd57b5c41a
trustyred <244073249@qq.com>
2018-04-14 11:25:25 +0800

    fix bike id bu

--- main_site/views.py
+++ main_site/views.py
@@ -114,7 +114,7 @@ def scanQr(request):
 def openBike(request):
     if request.method == 'POST':
         bike_id = request.POST.get('bike_id')
-        
+
         try:
             data = bikeData.objects.get(id=bike_id)
 
