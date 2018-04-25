0e82ca439104369af7118aa0afa5bfb4db2a2b6d
trustyred <244073249@qq.com>
2018-04-25 15:53:03 +0800

    增加了向后端请求单车历史数据点的函数

--- main_site/static/js/detail.js
+++ main_site/static/js/detail.js
@@ -4,26 +4,52 @@
 * @Last Modified by:   Administrator
 * @Last Modified time: 2018-04-19 15:58:15
 */
+var bikeId = getQueryString('bikeId');
+var tripId = getQueryString('tripId');
+var startTime = getQueryString('startTime');
+var cycling_time = getQueryString('cycling_time');
+var startDate = getQueryString('startDate');
+
+
 function getQueryString(name) {
 var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
 var r = window.location.search.substr(1).match(reg);
 if (r != null) return unescape(r[2]); return null;
+}
+
+function getBikePoint(){
+
+
+	$.ajax({
+		type:'POST',
+		url:'/main/getBikeHistoryPoint/',
+		data:{
+			'tripId':tripId
+		},
+		success:function (data) {
+			console.log(data)
+
+        },
+        error:function (e) {
+			console.log("getBikeHistoryPoint : "+e);
+        }
+
+
+	})
+
 
 }
 $(document).ready(function () {
 	var map = new BMap.Map("map-detail");    // 创建Map实例
 	map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
-	var bikeId = getQueryString('bikeId');
-	var tripId = getQueryString('tripId');
-	var startTime = getQueryString('startTime');
-	var cycling_time = getQueryString('cycling_time');
-	var startDate = getQueryString('startDate');
+
 
 	console.log(startDate);
 	$('#bike-id').text(bikeId);
 	$('#start-time').text(startTime);
 	$('#cycling-time').text(cycling_time);
 	$('#current-time').text(startDate);
+	getBikePoint();
 
 }
 );
