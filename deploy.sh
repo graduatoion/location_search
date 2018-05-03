ec54ccfb7474064e3d7710896c4e717cbb9df09f
trustyred <244073249@qq.com>
2018-05-03 12:28:43 +0800

    部署脚本

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,14 @@ STATIC_ROOT=$(cat location_search/settings.py|grep STATIC_ROOT | cut -d ' ' -f 3
 rm -rf -v $STATIC_ROOT/*
 #部署当前的静态文件
 python ./manager.py collectstatic
+#关闭当前的uwsgi服务
+killall -9 uwsgi
 #运行uwsgi服务
-uwsgi ./${UWSGI_INI}
+uwsgi_nums=$(ps aux|grep uwsgi | grep -v "grep" | wc -l)
+if [ $uwsgi_nums -eq 0 ];then
+    uwsgi ./${UWSGI_INI} 2&>1 /dev/null
+else
+	echo "uwsgi already exist"
+fi
 
 
